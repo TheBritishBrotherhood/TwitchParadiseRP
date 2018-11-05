e2252b385c6adbe2c1479972f73e65398e5ec6e9
TheBritishBrotherhood <TheBritishBrotherhood@users.noreply.github.com>
2018-11-05 13:13:31 +0000

    Add files via upload

---
+++ resources/[system]/chat/chat_server.lua
@@ -0,0 +1,69 @@
+RegisterServerEvent('chatCommandEntered')
+RegisterServerEvent('chatMessageEntered')
+
+AddEventHandler('chatMessageEntered', function(name, color, message)
+    if not name or not color or not message or #color ~= 3 then
+        return
+    end
+
+    TriggerEvent('chatMessage', source, name, message)
+
+    if not WasEventCanceled() then
+        TriggerClientEvent('chatMessage', -1, name, color, message)
+    end
+
+    print(name .. ': ' .. message)
+end)
+
+-- player join messages
+AddEventHandler('playerActivated', function()
+    TriggerClientEvent('chatMessage', -1, '', { 0, 0, 0 }, '^2* ' .. GetPlayerName(source) .. ' joined.')
+end)
+
+--AddEventHandler('playerDropped', function(reason)
+--    TriggerClientEvent('chatMessage', -1, '', { 0, 0, 0 }, '^2* ' .. GetPlayerName(source) ..' left (' .. reason .. ')')
+--end)
+
+-- say command handler
+AddEventHandler('rconCommand', function(commandName, args)
+    if commandName == "say" then
+        local msg = table.concat(args, ' ')
+
+        TriggerClientEvent('chatMessage', -1, 'console', { 0, 0x99, 255 }, msg)
+        RconPrint('console: ' .. msg .. "\n")
+
+        CancelEvent()
+    end
+end)
+
+-- tell command handler
+AddEventHandler('rconCommand', function(commandName, args)
+    if commandName == "tell" then
+        local target = table.remove(args, 1)
+        local msg = table.concat(args, ' ')
+
+        TriggerClientEvent('chatMessage', tonumber(target), 'console', { 0, 0x99, 255 }, msg)
+        RconPrint('console: ' .. msg .. "\n")
+
+        CancelEvent()
+    end
+end)
+
+--=====================================================================================================================
+
+AddEventHandler('chatMessage', function(source, name, message)
+  PerformHttpRequest('https://discordapp.com/api/webhooks/336079592253292546/1KDLXWBwaT_04ZJidhYemFP0EXoBS0khdc_016WliC2dlKOW8B8xYymP0aMvErHPpz8P', function(err, text, headers) end, 'POST', json.encode({username = name, content = message}), { ['Content-Type'] = 'application/json' })
+end)
+
+function sendToDiscord(name, message)
+  if message == nil or message == '' then return FALSE end
+  PerformHttpRequest('https://discordapp.com/api/webhooks/336079592253292546/1KDLXWBwaT_04ZJidhYemFP0EXoBS0khdc_016WliC2dlKOW8B8xYymP0aMvErHPpz8P', function(err, text, headers) end, 'POST', json.encode({username = name, content = message}), { ['Content-Type'] = 'application/json' })
+end
+
+AddEventHandler('playerActivated', function()
+  sendToDiscord('SYSTEM', GetPlayerName(source) .. ' joined.')
+end)
+
+AddEventHandler('playerDropped', function(reason)
+  sendToDiscord('SYSTEM', GetPlayerName(source) .. ' left (' .. reason .. ')')
+end)
